0bc121fcefee9c21c83a8386012d9540cc0446b2
theweakant <nam503888@gmail.com>
2025-07-12 23:02:38 +0700

    check role for impor export product

--- routes/batch/batchStock.routes.js
+++ routes/batch/batchStock.routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const batchStockController = require("../../controllers/batch/batchStock.controller");
 const authMiddleware = require("../../middlewares/auth/auth.middleware");
+const { authorizeAdmin } = require("../../middlewares/auth/role.middleware");
 
-router.post("/", authMiddleware, batchStockController.createBatchStock);
-router.get("/", authMiddleware, batchStockController.getAllBatchStocks);
-router.get("/:batchStockId", authMiddleware,  batchStockController.getBatchStockById);
+router.post("/", authMiddleware, authorizeAdmin, authorizeStaff, batchStockController.createBatchStock);
+router.get("/", authMiddleware, authorizeAdmin, batchStockController.getAllBatchStocks);
+router.get("/:batchStockId", authMiddleware, authorizeAdmin,  batchStockController.getBatchStockById);
 // router.put("/:batchStockId", authMiddleware, batchStockController.updateBatchStock);
 // router.delete("/:batchStockId", authMiddleware, batchStockController.deleteBatchStock);
 
--- routes/warehouse/warehouse.routes.js
+++ routes/warehouse/warehouse.routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const warehouseController = require("../../controllers/warehouse/warehouse.controller");
 
-router.post("/", warehouseController.createWarehouse);
-router.get("/", warehouseController.getAllWarehouses);
-router.get("/:warehouseId", warehouseController.getWarehouseById);
-router.put("/:warehouseId", warehouseController.updateWarehouse);
+const authMiddleware = require("../../middlewares/auth/auth.middleware");
+const { authorizeAdmin } = require("../../middlewares/auth/role.middleware");
 
-router.delete("/:warehouseId", warehouseController.deleteWarehouse);
+router.post("/", authMiddleware, authorizeAdmin, warehouseController.createWarehouse);
+router.get("/", authMiddleware, authorizeAdmin, warehouseController.getAllWarehouses);
+router.get("/:warehouseId", authMiddleware, authorizeAdmin, warehouseController.getWarehouseById);
+router.put("/:warehouseId", authMiddleware, authorizeAdmin, warehouseController.updateWarehouse);
+router.delete("/:warehouseId", authMiddleware, authorizeAdmin, warehouseController.deleteWarehouse);
 
 module.exports = router;
